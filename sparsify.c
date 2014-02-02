ad8e747937ad98db60ecd4e1c43907fec6a45113
cheater <195390+cheater@users.noreply.github.com>
2014-02-02 16:12:18 +0100

    Released under GNU GPL v3.

--- sparsify.c
+++ sparsify.c
@@ -11,6 +11,22 @@
      
     #define BLOCKSIZE 4096
      
+//LICENSE
+//
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 //supplied by remcohn from #btfs on Freenode
 //originally punch.c
 //but sparsify is easier to remember
